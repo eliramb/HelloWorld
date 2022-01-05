3a6fdf31b354efab6e861d12ede7cf7f914bce56
Eliram Benitah <eliram.benitah@gmail.com>
2022-01-05 20:44:35 +0200

    Hellow world2

--- src/com/example/helloWorld/HellowWorld.java
+++ src/com/example/helloWorld/HellowWorld.java
@@ -2,6 +2,6 @@ package com.example.helloWorld;
 
 public class HellowWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Israel and jerusalem1");
+        System.out.println("Hello, Israel and jerusalem2");
     }
 }
